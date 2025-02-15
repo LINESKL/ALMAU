87e40b88b405d0aec5cdeb0da55d45280f92d2af
LINESKL <priveth06@gmail.com>
2025-02-15 10:08:46 +0500

    3 commit

--- hw3/TODOS/todosapp/forms.py
+++ hw3/TODOS/todosapp/forms.py
@@ -5,3 +5,9 @@ class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ['title', 'description', 'due_date', 'status']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter description', 'rows': 2}),
+            'due_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'status': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
