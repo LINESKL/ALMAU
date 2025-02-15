911a1af2bd53572282a4087029260e6347ab8e14
LINESKL <priveth06@gmail.com>
2025-02-15 09:09:48 +0500

    third commit

--- hw3/TODOS/todosapp/views.py
+++ hw3/TODOS/todosapp/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
+from django.http import Http404
 from .models import Todo
 from .forms import TodoForm
+
 
 def todo_list(request):
     todos = Todo.objects.all()
     return render(request, 'todos/todo_list.html', {'todos': todos})
 
+
 def todo_detail(request, id):
-    todo = get_object_or_404(Todo, id=id)
+    try:
+        todo = Todo.objects.get(id=id)
+    except Todo.DoesNotExist:
+        raise Http404("Todo not found")
     return render(request, 'todos/todo_detail.html', {'todo': todo})
+
 
 def todo_create(request):
     if request.method == 'POST':
@@ -20,8 +27,13 @@ def todo_create(request):
         form = TodoForm()
     return render(request, 'todos/todo_form.html', {'form': form})
 
+
 def todo_edit(request, id):
-    todo = get_object_or_404(Todo, id=id)
+    try:
+        todo = Todo.objects.get(id=id)
+    except Todo.DoesNotExist:
+        raise Http404("Todo not found")
+
     if request.method == 'POST':
         form = TodoForm(request.POST, instance=todo)
         if form.is_valid():
@@ -31,11 +43,14 @@ def todo_edit(request, id):
         form = TodoForm(instance=todo)
     return render(request, 'todos/todo_form.html', {'form': form, 'is_edit': True})
 
+
 def todo_delete(request, id):
-    todo = get_object_or_404(Todo, id=id)
-    render(request, 'todos/todo_confirm_delete.html', {'todo': todo})
+    try:
+        todo = Todo.objects.get(id=id)
+    except Todo.DoesNotExist:
+        raise Http404("Todo not found")
+
     if request.method == 'POST':
         todo.delete()
         return redirect('todo_list')
-
-
+    return redirect('todo_list')
